86eec4303c4fdbf28f97c29506be57837ee4385f
James Garinger <jamesg@Jamess-MacBook-Air.local>
2022-12-18 15:49:08 -0600

    connect to xrpl, listen for all transactions to a specified wallet by subscribing and filter transactions with TransactionType 'Payment'

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # xrpl_subscribe_to_account
 listen for specific transactions to a specified wallet or wallets
+
+First, after cloning the repo, use npm i to install xrpl.js library.
+
+Then, simply just add the wallet r address to ADDRESS variable that you would like to track payments to.
+
+use 'node index.js' to run the program and start listening for transactions to that wallet.
+
+use ctr + c to end the program from continuously running, or add a timeout function to listen for only a set amount of time and manually disconnect.
+
--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-var xrpl = require('xrpl')
+const xrpl = require('xrpl')
 require('dotenv').config()
 
 
+async function main() {
+    // Set the address of the wallet to monitor
+    const ADDRESS = process.env.WALLET_TWO_R
+
+    const client = new xrpl.Client('wss://s.altnet.rippletest.net:51233');
+    await client.connect();
+
+    const stream = await client.request({
+        command: 'subscribe',
+        accounts: [ADDRESS]
+
+    });
+
+    console.log("This is my stream response: ", stream);
+
+    client.on('transaction', (tx) => {
+        if (tx.transaction.TransactionType === 'Payment' && tx.transaction.Destination === ADDRESS) {
+            console.log('Moniored Address Received a Payment!');
+            console.log(`
+                Payment Info:
+                Transaction: ${tx.transaction.TransactionType},
+                Payment Status: ${tx.meta.TransactionResult},
+                Sender: ${tx.transaction.Account},
+                Receiver: ${tx.transaction.Destination},
+                Amount: ${tx.transaction.Amount / 1000000} XRP
+            `)
+
+        };
+    });
+
+}
+
+main();
